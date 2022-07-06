b59eaca216b7004291949ed9f88141757eb4cf6f
Rasulibra1 <Rasulibra1@csu.fullerton.edu>
2022-07-06 02:05:26 -0700

    merge conflict

--- code-test.R
+++ code-test.R
@@ -1,9 +1,10 @@
-setwd("")
+setwd("/Users/rasulibragimov/Cal State Fullerton/Jaynes, Jessica - Group2")
 dir()
 load("cfCOVIDgroup6.RDATA")
 
 View(d)
 library(tidyverse)
+library(janitor)
 
 choc <- d
 
@@ -14,3 +15,7 @@ summarise(mean(choc$bmi_percentile))
 all_comorb <- select(choc, contains("comorb"))
 
 glimpse(all_comorb)
+
+d %>% 
+  group_by(gender) %>% 
+  print()
